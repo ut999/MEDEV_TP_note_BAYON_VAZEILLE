d750dacfc793c420338e743d67fb558abf11a69c
utaBdN <utaabn@gmail.com>
2026-01-11 23:14:21 +0100

    Create Maven project

---
+++ Projet_Maven/src/main/java/ecn/infosi/projet_maven/Projet_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ecn.infosi.projet_maven;
+
+/**
+ *
+ * @author utaab
+ */
+public class Projet_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
