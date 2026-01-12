83291305a4ae533b1485348d7e1c912482a4eacf
clement-vazeille <clement.vazeille@gmail.com>
2026-01-12 15:29:00 +0100

    Gestion des inputs terminée

--- Projet_Maven/src/main/java/ecn/infosi/projet_maven/Input.java
+++ Projet_Maven/src/main/java/ecn/infosi/projet_maven/Input.java
@@ -14,6 +14,7 @@ public class Input {
     
     /**
      * Ask and return a character as a try for the player, needs the character to be different than precedent tries
+     * @param previousTries list containing precedent tries
      * @return characters written by the player
      */
     public String readPlayerCharacterTry(ArrayList<String> previousTries)
@@ -56,6 +57,7 @@ public class Input {
     
     /**
      *  Ask and return a character as a try for the player, needs to be shorten than a given limit
+     * @param maxSize max world size
      * @return mystery word written by the player
      */
     public String readMysteryWord(int maxSize)
@@ -104,38 +106,117 @@ public class Input {
     }
     
     /**
-     *
-     * @return
+     *  Ask player for max errors allowed
+     * @param maxAllowed max input possible
+     *  @return result between 1 and intput
      */
-    public int readMaxErrorsAllowed()
+    public int readMaxErrorsAllowed(int maxAllowed)
     {
-        return 7;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        while(true)
+        {
+            boolean isInputCorrect = true;
+            System.out.println("Veuillez entrer le nombre maximum d'erreur avant une defaite");
+            System.out.println("L'input doit etre entre 1 et " + maxAllowed);
+
+            int numInput = myObj.nextInt();  // Read user input
+             
+            if(numInput < 1 || numInput > maxAllowed)
+            {
+                System.out.println("Votre nombre est inferieur a 1 ou superieur au maximum autorisee");
+                isInputCorrect=false;
+            }
+            
+            if(isInputCorrect)
+            {
+                return numInput; 
+            }
+        }
     }
     
     /**
-     *
-     * @return
+     * Ask player for gamemode, 0 for player vs computer, 1 for player vs player
+     * @return 0 for player vs computer, 1 for player vs player
      */
     public int chooseGameMode()
     {
-        return 0;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        while(true)
+        {
+            boolean isInputCorrect = true;
+            System.out.println("Veuillez choisir le mode de jeu (0 : jouer contre l'ordinateur, 1 : jouer contre un autre joueur");
+
+            int numInput = myObj.nextInt();  // Read user input
+             
+            if(numInput < 0 || numInput > 1)
+            {
+                System.out.println("Veuillez entrer 1 ou 0");
+                isInputCorrect=false;
+            }
+            
+            if(isInputCorrect)
+            {
+                return numInput; 
+            }
+        }
     }
     
     /**
-     *
-     * @return
+     * Ask for the max word size allowed
+     * @return max word size allowed
      */
     public int readMaxWordSize()
     {
-        return 0;
+                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        while(true)
+        {
+            boolean isInputCorrect = true;
+            System.out.println("Veuillez choisir la taille maximale du mot que vous pourrez deviner");
+
+            int numInput = myObj.nextInt();  // Read user input
+             
+            if(numInput < 1)
+            {
+                System.out.println("Veuillez entrer un nombre strictement positive");
+                isInputCorrect=false;
+            }
+            
+            if(isInputCorrect)
+            {
+                return numInput; 
+            }
+        }
     }
     
     /**
-     *
-     * @return
+     * Ask player for the dictionnaire file
+     * @return dictionnaire file
      */
     public String inputDictionnaireName()
     {
-        return "";
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        
+        while(true)
+        {
+            boolean isInputCorrect = true;
+            System.out.println("Veuillez entrer le nom du dictionnaire à utiliser (init.txt est le dictionnaire de base)");
+
+            String wordInput = myObj.nextLine();  // Read user input
+             
+            if(wordInput.length()== 0)
+            {
+                System.out.println("nom de fichier est vide");
+                isInputCorrect=false;
+            }
+            
+            
+            if(isInputCorrect)
+            {
+                return wordInput;
+            }
+        }
     }
 }
