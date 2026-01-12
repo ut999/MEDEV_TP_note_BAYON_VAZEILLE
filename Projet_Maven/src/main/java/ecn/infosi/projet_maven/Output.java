02a41a18adcbc3d109f7f31bc5cb20614889de3f
clement-vazeille <clement.vazeille@gmail.com>
2026-01-12 16:22:27 +0100

    Output fonctionnel
    Les fonctions d'out devrait être fonctionnelles

--- Projet_Maven/src/main/java/ecn/infosi/projet_maven/Output.java
+++ Projet_Maven/src/main/java/ecn/infosi/projet_maven/Output.java
@@ -11,28 +11,82 @@ package ecn.infosi.projet_maven;
 public class Output {  
     public void affichageDebutJeu()
     {
+        System.out.println("Bienvenue dans notre jeu du pendu");
+        System.out.println("Il y a 2 modes de jeu differents");
+        System.out.println("1. Jouer contre l'ordinateur, un mot est choisi au hasard parmis le dictionnaire de ton choix et tu dois le deviner.");
+        System.out.println("2. Jouer contre un autre joueur, un premier joueur ecris un mot puis un second doit devenir  ce mot.");
     }
     
     public void affichageTourJeu(EtatJeu etatJeu)
     {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println("------------------------------------");
+        System.out.println("Tour n°" + etatJeu.getCurrentTurn());
+        
+        
+        System.out.println("nombre d'erreurs commises : " + etatJeu.getWordToGuess());
+        System.out.println("nombre d'erreurs maximum : " + etatJeu.getMaxErrors());
         dessinPendu(etatJeu.getCurrentError(), etatJeu.getMaxErrors());
-        System.out.println("mot actuel : ");
-        for(String s : etatJeu.getGuessedLetters())
+        System.out.println("mot actuel : " + etatJeu.getShowGuessedLetters());
+        
         
         System.out.println("Liste des lettres deja donnees : ");
+        System.out.println();
+        for(String s : etatJeu.getGuessedLetters())
+        {
+            System.out.print(s);
+        }
+        
         
     }
     
     public void affichageVictoire(EtatJeu etatJeu)
     {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("------------------------------------");
+        System.out.println("Felicitation tu as gagne!");
+        System.out.println("Tu as gagne au tour n°" + etatJeu.getCurrentTurn());
+        System.out.println("le mot etait : " + etatJeu.getWordToGuess());
+        System.out.println("tu est alle jusque la : " + etatJeu.getShowGuessedLetters());
+        
+        System.out.println("nombre d'erreurs commises : " + etatJeu.getWordToGuess());
+        System.out.println("nombre d'erreurs maximum : " + etatJeu.getMaxErrors());
+        
+        dessinPendu(etatJeu.getCurrentError(), etatJeu.getMaxErrors());
         
     }
     
     public void affichageDefaite(EtatJeu etatJeu)
     {
-
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("------------------------------------");
+        System.out.println("Dommage tu as perdu :(");
+        System.out.println("Tu as perdu n°" + etatJeu.getCurrentTurn());
+        System.out.println("le mot etait : " + etatJeu.getWordToGuess());
         
+        
+        dessinPendu(etatJeu.getCurrentError(), etatJeu.getMaxErrors());   
     }
     
     private void dessinPendu(double nbErreurs, double nbErreursMax)
