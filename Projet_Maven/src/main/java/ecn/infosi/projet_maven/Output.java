acd167b8bc1899e19d365c568b6271eff331fb80
clement-vazeille <clement.vazeille@gmail.com>
2026-01-12 15:47:12 +0100

    Squelette de Output
    Création des définitions des fonctions de output, (pas encore implémentée)

--- Projet_Maven/src/main/java/ecn/infosi/projet_maven/Output.java
+++ Projet_Maven/src/main/java/ecn/infosi/projet_maven/Output.java
@@ -8,6 +8,51 @@ package ecn.infosi.projet_maven;
  *
  * @author Clément
  */
-public class Output {
+public class Output {  
+    public void affichageDebutJeu()
+    {
+        
+    }
     
+    public void affichageTourJeu(EtatJeu etatJeu)
+    {
+        
+    }
+    
+    public void affichageVictoire(EtatJeu etatJeu)
+    {
+        
+    }
+    
+    public void affichageDefaite(EtatJeu etatJeu)
+    {
+
+        
+    }
+    
+    private void dessinPendu(double nbErreurs, double nbErreursMax)
+    {
+        if(nbErreurs >= nbErreursMax-0.001)
+        {
+            System.out.println(" +---+\n" +
+                               " |   |\n" +
+                               " O   |\n" +
+                               "/|\\  |\n" +
+                               " |   |\n" +
+                               "/ \\  |\n" +
+                               "=======");
+            return;
+        }
+        if(nbErreurs >= nbErreursMax-0.001)
+        {
+            System.out.println(" +---+\n" +
+                               " |   |\n" +
+                               " O   |\n" +
+                               "/|\\  |\n" +
+                               " |   |\n" +
+                               "/ \\  |\n" +
+                               "=======");
+            return;
+        }
+    }
 }
