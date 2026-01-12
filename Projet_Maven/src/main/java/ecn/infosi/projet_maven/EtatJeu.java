10a19ad7e86923c09043dee227b48b06df8f3b6a
utaBdN <utaabn@gmail.com>
2026-01-12 17:32:24 +0100

    Update EtatJeu.java
    Add javadoc

--- Projet_Maven/src/main/java/ecn/infosi/projet_maven/EtatJeu.java
+++ Projet_Maven/src/main/java/ecn/infosi/projet_maven/EtatJeu.java
@@ -7,7 +7,7 @@ package ecn.infosi.projet_maven;
 import java.util.ArrayList;
 
 /**
- *
+ * This is a class to manage every game variable and data.
  * @author utaab
  */
 public class EtatJeu {
@@ -25,6 +25,9 @@ public class EtatJeu {
     private String showGuessedLetters;
     private int gameState;
 
+    /**
+     * Constructor with initial parameters
+     */
     public EtatJeu() {
         this.guessedLetters = new ArrayList<>();
         this.wordLetters = new ArrayList<>();
@@ -40,98 +43,194 @@ public class EtatJeu {
         this.gameState = 0;
     }
 
+    /**
+     * Getter of guessed letters
+     * @return
+     */
     public ArrayList<String> getGuessedLetters() {
         return guessedLetters;
     }
 
+    /**
+     * Setter of guessed letters
+     * @param guessedLetters
+     */
     public void setGuessedLetters(ArrayList<String> guessedLetters) {
         this.guessedLetters = guessedLetters;
     }
 
+    /**
+     * Getter of word letters
+     * @return
+     */
     public ArrayList<String> getWordLetters() {
         return wordLetters;
     }
 
+    /**
+     * Setter of word letters
+     * @param wordLetters
+     */
     public void setWordLetters(ArrayList<String> wordLetters) {
         this.wordLetters = wordLetters;
     }
 
+    /**
+     * Getter of current error
+     * @return
+     */
     public int getCurrentError() {
         return currentError;
     }
 
+    /**
+     * Setter of current error
+     * @param currentError
+     */
     public void setCurrentError(int currentError) {
         this.currentError = currentError;
     }
 
+    /**
+     * Getter of max error choosed
+     * @return
+     */
     public int getMaxErrors() {
         return maxErrors;
     }
 
+    /**
+     * Setter of max error choosed
+     * @param maxErrors
+     */
     public void setMaxErrors(int maxErrors) {
         this.maxErrors = maxErrors;
     }
 
+    /**
+     * Getter of max error allowed
+     * @return
+     */
     public int getMaxAllowed() {
         return maxAllowed;
     }
 
+    /**
+     * Setter of max error allowed
+     * @param maxErrors
+     */
     public void setMaxAllowed(int maxErrors) {
         this.maxAllowed = maxErrors;
     }
     
+    /**
+     * Getter of current turn
+     * @return
+     */
     public int getCurrentTurn() {
         return currentTurn;
     }
 
+    /**
+     * Setter of current turn
+     * @param currentTurn
+     */
     public void setCurrentTurn(int currentTurn) {
         this.currentTurn = currentTurn;
     }
 
+    /**
+     * Getter of max turns
+     * @return
+     */
     public int getMaxTurns() {
         return maxTurns;
     }
 
+    /**
+     * Setter of max turns
+     * @param maxTurns
+     */
     public void setMaxTurns(int maxTurns) {
         this.maxTurns = maxTurns;
     }
 
+    /**
+     * Getter of word to guess
+     * @return
+     */
     public String getWordToGuess() {
         return wordToGuess;
     }
 
+    /**
+     * Setter of word to guess
+     * @param wordToGuess
+     */
     public void setWordToGuess(String wordToGuess) {
         this.wordToGuess = wordToGuess;
     }
 
+    /**
+     * Getter of maximum word size
+     * @return
+     */
     public int getMaxWordSize() {
         return maxWordSize;
     }
 
+    /**
+     * Setter of maximum word size
+     * @param maxWordSize
+     */
     public void setMaxWordSize(int maxWordSize) {
         this.maxWordSize = maxWordSize;
     }
 
+    /**
+     * Getter of gamemode
+     * @return
+     */
     public int getGameMode() {
         return gameMode;
     }
 
+    /**
+     * Setter of gamemode
+     * @param gameMode
+     */
     public void setGameMode(int gameMode) {
         this.gameMode = gameMode;
     }
 
+    /**
+     * Getter of show guessed letters
+     * @return
+     */
     public String getShowGuessedLetters() {
         return showGuessedLetters;
     }
 
+    /**
+     * Setter of show guessed letters
+     * @param showGuessedLetters
+     */
     public void setShowGuessedLetters(String showGuessedLetters) {
         this.showGuessedLetters = showGuessedLetters;
     }
 
+    /**
+     * Getter of gamestate
+     * @return
+     */
     public int getGameState() {
         return gameState;
     }
 
+    /**
+     * Setter of gamestate
+     * @param gameState
+     */
     public void setGameState(int gameState) {
         this.gameState = gameState;
     }
